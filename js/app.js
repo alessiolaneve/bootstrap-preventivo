55b6a67436af84fab2e4c094f590561a7bed1ce1
alessiolaneve <alessioln98@gmail.com>
2024-04-07 20:17:36 +0200

    progetto finito (fixare breakpoints e validazioni)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-console.log('ciao mondo')
-
 // Aggiungiamo la componente js di interazione con l’utente.
 // Quando l’utente fa click sul bottone submit del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste. 
 
@@ -13,35 +11,69 @@ console.log('ciao mondo')
 
 // assegnare le variabili
 
-const codPromo = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
+const codiciPromo = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+let sconto = 0;
 
-const btn = document.getElementById('btnCalc')
-const printPrice = document.getElementById('price')
+const btn = document.getElementById('btnCalc');
+const printPrice = document.getElementById('price');
 
-const backPrice = 20.50
-const frontPrice = 15.30
-const projPrice = 33.60
+const backPrice = 20.50;
+const frontPrice = 15.30;
+const projPrice = 33.60;
 
 
 
-function calcPrev() {
+function calcPrev(event) {
+    event.preventDefault();
+    calcSconto();
+
     const job = document.getElementById('inputJob').selectedIndex;
-    console.log(job);
+    // console.log(job);
 
     const prices = [backPrice, frontPrice, projPrice];
 
     if (job == 0) {
-        alert('Non hai selezionato un lavoro');
+
+        alert('Non hai selezionato un lavoro.');
+
     } else if (job > 0 && job <= prices.length) {
-        price = (prices[job - 1] * 10).toFixed(2);
+        let price = (prices[job - 1] * 10);
         console.log(price);
-        printPrice.innerHTML = 'Prezzo finale: € ' + price;
+
+        let priceSconto = price * sconto;
+        let priceTot = price - priceSconto;
+
+        printPrice.innerHTML = 'Prezzo finale: € ' + priceTot;
+
     }
 }
 
 btn.addEventListener('click', calcPrev)
 
+// PER CALCOLARE LO SCONTO
+// trasformare la stringa in caps
+// controllare che il codice sia corretto
+// applicare sconto
 
+const inputPromo = document.getElementById('cod_promo').value;
+// console.log(inputPromo);
+const capsPromo = inputPromo.toUpperCase();
+// console.log(capsPromo);
+
+
+function calcSconto() {
+    for (let i = 0; i < codiciPromo.length; i++) {
+        const codice = codiciPromo[i];
+        console.log(codice)
+
+        if (capsPromo == codice) {
+            sconto = 0.25;
+            break;
+        }
+    }   if (sconto != 0.25) {
+        alert('Il codice promozionale inserito non è valido!')
+    }
+}
 
 
 // L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti: YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24.
